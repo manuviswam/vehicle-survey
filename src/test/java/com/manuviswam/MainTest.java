2d0d9a1cfe7efb62dd4aa933dc371d91bc872b1c
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-10 23:02:29 +0530

    Initial project structure

---
+++ src/test/java/com/manuviswam/MainTest.java
@@ -0,0 +1,13 @@
+package com.manuviswam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        assertTrue(true);
+    }
+}
