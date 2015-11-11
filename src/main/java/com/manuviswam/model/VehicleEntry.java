1e9814f0d35c228c7ef11aadf2ddd5fc1107b4d4
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-11 12:00:56 +0530

    Better variable names in VehicleEntry model

--- src/main/java/com/manuviswam/model/VehicleEntry.java
+++ src/main/java/com/manuviswam/model/VehicleEntry.java
@@ -1,14 +1,14 @@
 package com.manuviswam.model;
 
 public class VehicleEntry {
-    private int firstAxleTime;
-    private int secondAxleTime;
+    private int frontAxleTime;
+    private int rearAxleTime;
 
     public Direction direction;
 
-    public VehicleEntry(int firstAxleTime, int secondAxleTime, Direction direction) {
-        this.firstAxleTime = firstAxleTime;
-        this.secondAxleTime = secondAxleTime;
+    public VehicleEntry(int frontAxleTime, int rearAxleTime, Direction direction) {
+        this.frontAxleTime = frontAxleTime;
+        this.rearAxleTime = rearAxleTime;
         this.direction = direction;
     }
 }
