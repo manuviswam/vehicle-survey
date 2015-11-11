8cbccb4bbb6bd67385bc8663b4b70f79eebed502
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-11 15:44:08 +0530

    Milliseconds to hours converter

--- src/main/java/com/manuviswam/helpers/Time.java
+++ src/main/java/com/manuviswam/helpers/Time.java
@@ -2,7 +2,6 @@ package com.manuviswam.helpers;
 
 public class Time {
 
-
     public static int parseMilliSecondFromInput(String input){
         try {
             int milliSeconds = Integer.parseInt(input.substring(1));
@@ -13,5 +12,14 @@ public class Time {
             System.out.println("Error parsing time from : " + input);
         }
         return -1;
+    }
+
+    public static double convertToHour(int milliseconds){
+        if (milliseconds <0 )
+            return -1;
+        int minutesInHour = com.manuviswam.constants.Time.MINUTES_IN_A_HOUR;
+        int secondsInMinute = com.manuviswam.constants.Time.SECONDS_IN_A_MINUTE;
+        int milliSecondsInSecond = com.manuviswam.constants.Time.MILLISECONDS_IN_A_SECOND;
+        return milliseconds/(milliSecondsInSecond * secondsInMinute * minutesInHour);
     }
 }
--- src/test/java/com/manuviswam/helpers/TimeTest.java
+++ src/test/java/com/manuviswam/helpers/TimeTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TimeTest {
+    private static final double DELTA = 1e-15;
 
     @Test
     public void shouldReturnMillisecondForValidInput() throws Exception {
@@ -22,5 +23,17 @@ public class TimeTest {
     public void shouldReturnMinusOneForLargeInput() throws Exception {
         int output = Time.parseMilliSecondFromInput("A123456789123456789123456789");
         assertEquals(-1,output);
+    }
+
+    @Test
+    public void shouldConvertMillisecondsToHours() throws Exception {
+        double output = Time.convertToHour(3600000);
+        assertEquals(1, output, DELTA);
+    }
+
+    @Test
+    public void shouldReturnMinusOneForNegativeInputs() throws Exception {
+        double output = Time.convertToHour(-1);
+        assertEquals(-1, output, DELTA);
     }
 }
