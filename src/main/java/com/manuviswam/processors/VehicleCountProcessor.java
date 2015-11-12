ccb55e18ab4b081f844f3bf46b6c376084cc7ea7
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-12 21:26:33 +0530

    Take vehicle count in both directions seperately

--- src/main/java/com/manuviswam/processors/VehicleCountProcessor.java
+++ src/main/java/com/manuviswam/processors/VehicleCountProcessor.java
@@ -1,6 +1,7 @@
 package com.manuviswam.processors;
 
 import com.manuviswam.constants.App;
+import com.manuviswam.model.Direction;
 import com.manuviswam.model.SessionOfTheDay;
 import com.manuviswam.model.VehicleEntry;
 
@@ -21,14 +22,17 @@ public class VehicleCountProcessor extends SessionViceProcessor {
         StringBuilder sb = new StringBuilder();
 
         List<SessionOfTheDay> sessions = SessionOfTheDay.createSessionsWithInterval(interval);
+        if (sessions.size() < 1)
+            return "";
         for(SessionOfTheDay session : sessions){
             int totalCountAcrossDays = 0;
             List<VehicleEntry> sessionEntries = getEntriesInTheSession(entries, session);
             sb.append(session.toString());
             for (int day = 0; day < App.NUMBER_OF_DAYS; day++) {
-                int count = countEntriesForTheDay(sessionEntries,day);
-                sb.append(" | Day " + day + " Count=" + count);
-                totalCountAcrossDays += count;
+                int southCount = countEntriesForTheDayForDirection(sessionEntries, day, Direction.SOUTH);
+                int northCount = countEntriesForTheDayForDirection(sessionEntries, day, Direction.NORTH);
+                sb.append(" | Day " + day + " Count South = " + southCount + " North = " + northCount);
+                totalCountAcrossDays += southCount + northCount;
             }
             sb.append("\n");
             if (totalCountAcrossDays > peakVehicleCount){
@@ -40,7 +44,7 @@ public class VehicleCountProcessor extends SessionViceProcessor {
         return sb.toString();
     }
 
-    private int countEntriesForTheDay(List<VehicleEntry> sessionEntries, int day) {
-        return sessionEntries.stream().filter(entry -> entry.getDay() == day).collect(Collectors.toList()).size();
+    private int countEntriesForTheDayForDirection(List<VehicleEntry> sessionEntries, int day, Direction direction) {
+        return sessionEntries.stream().filter(entry -> entry.getDay() == day && entry.getDirection() == direction).collect(Collectors.toList()).size();
     }
 }
--- src/test/java/com/manuviswam/processors/VehicleCountProcessorTest.java
+++ src/test/java/com/manuviswam/processors/VehicleCountProcessorTest.java
@@ -13,46 +13,30 @@ import static org.junit.Assert.*;
 public class VehicleCountProcessorTest {
     private List<VehicleEntry> entries = new ArrayList<>();
     private String expectedOutput =
-            "Session 00:00:00 to 01:00:00 | Day 0 Count=2 | Day 1 Count=3 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 01:00:00 to 02:00:00 | Day 0 Count=1 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 02:00:00 to 03:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 03:00:00 to 04:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 04:00:00 to 05:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 05:00:00 to 06:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 06:00:00 to 07:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 07:00:00 to 08:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 08:00:00 to 09:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 09:00:00 to 10:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 10:00:00 to 11:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 11:00:00 to 12:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 12:00:00 to 13:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 13:00:00 to 14:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 14:00:00 to 15:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 15:00:00 to 16:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 16:00:00 to 17:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 17:00:00 to 18:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 18:00:00 to 19:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 19:00:00 to 20:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 20:00:00 to 21:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 21:00:00 to 22:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 22:00:00 to 23:00:00 | Day 0 Count=0 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "Session 23:00:00 to 00:00:00 | Day 0 Count=1 | Day 1 Count=0 | Day 2 Count=0 | Day 3 Count=0 | Day 4 Count=0\n" +
-            "\nSession 00:00:00 to 01:00:00 is peak session with a vehicle count of 5 across 5 days";
+            "Session 00:00:00 to 06:00:00 | Day 0 Count South = 2 North = 2 | Day 1 Count South = 1 North = 3 | Day 2 Count South = 0 North = 0 | Day 3 Count South = 0 North = 0 | Day 4 Count South = 0 North = 0\n" +
+            "Session 06:00:00 to 12:00:00 | Day 0 Count South = 0 North = 0 | Day 1 Count South = 0 North = 0 | Day 2 Count South = 0 North = 0 | Day 3 Count South = 0 North = 0 | Day 4 Count South = 0 North = 0\n" +
+            "Session 12:00:00 to 18:00:00 | Day 0 Count South = 0 North = 0 | Day 1 Count South = 0 North = 0 | Day 2 Count South = 0 North = 0 | Day 3 Count South = 0 North = 0 | Day 4 Count South = 0 North = 0\n" +
+            "Session 18:00:00 to 00:00:00 | Day 0 Count South = 1 North = 0 | Day 1 Count South = 1 North = 0 | Day 2 Count South = 0 North = 0 | Day 3 Count South = 0 North = 0 | Day 4 Count South = 0 North = 0\n" +
+            "\nSession 00:00:00 to 06:00:00 is peak session with a vehicle count of 8 across 5 days";
 
     @Before
     public void setUp() throws Exception {
-        entries.add(new VehicleEntry(86328771,86328899, Direction.SOUTH,0));
+        entries.add(new VehicleEntry(8771,8899, Direction.SOUTH,0));
+        entries.add(new VehicleEntry(28771,28899, Direction.NORTH,0));
         entries.add(new VehicleEntry(328771,328899, Direction.SOUTH,0));
-        entries.add(new VehicleEntry(6328771,6328899, Direction.SOUTH,0));
-        entries.add(new VehicleEntry(28771,28899, Direction.SOUTH,0));
-        entries.add(new VehicleEntry(582668,582787, Direction.SOUTH,1));
-        entries.add(new VehicleEntry(82668,82787, Direction.SOUTH,1));
-        entries.add(new VehicleEntry(2668,2787, Direction.SOUTH,1));
+        entries.add(new VehicleEntry(6328771,6328899, Direction.NORTH,0));
+        entries.add(new VehicleEntry(86328771,86328899, Direction.SOUTH,0));
+
+        entries.add(new VehicleEntry(8632,8732, Direction.NORTH,1));
+        entries.add(new VehicleEntry(86328,86400, Direction.NORTH,1));
+        entries.add(new VehicleEntry(863287,863328, Direction.SOUTH,1));
+        entries.add(new VehicleEntry(8632877,8632889, Direction.NORTH,1));
+        entries.add(new VehicleEntry(86328771,86328899, Direction.SOUTH,1));
     }
 
     @Test
     public void shouldGiveSessionviceOutputForHourlyCount() throws Exception {
-        VehicleCountProcessor processor = new VehicleCountProcessor(60);
+        VehicleCountProcessor processor = new VehicleCountProcessor(360);
         String output = processor.process(entries);
 
         assertEquals(expectedOutput, output);
@@ -60,10 +44,18 @@ public class VehicleCountProcessorTest {
 
     @Test
     public void shouldGivePeakSessionInformationInOutput() throws Exception {
-        VehicleCountProcessor processor = new VehicleCountProcessor(60);
+        VehicleCountProcessor processor = new VehicleCountProcessor(360);
         String output = processor.process(entries);
 
-        assertTrue(output.contains("Session 00:00:00 to 01:00:00 is peak session with a vehicle count of 5 across 5 days"));
+        assertTrue(output.contains("Session 00:00:00 to 06:00:00 is peak session with a vehicle count of 8 across 5 days"));
+    }
+
+    @Test
+    public void shouldHaveSeperateCountForBothDirections() throws Exception {
+        VehicleCountProcessor processor = new VehicleCountProcessor(360);
+        String output = processor.process(entries);
+
+        assertTrue(output.contains("Day 0 Count South = 2 North = 2"));
     }
 
     @Test
