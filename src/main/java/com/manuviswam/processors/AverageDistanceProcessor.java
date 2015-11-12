c73e7a7fc407a9df3b2c41d07ad8b4b46c44b73f
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-12 21:37:04 +0530

    Refactored AverageDistanceProcessor

--- src/main/java/com/manuviswam/processors/AverageDistanceProcessor.java
+++ src/main/java/com/manuviswam/processors/AverageDistanceProcessor.java
@@ -21,15 +21,18 @@ public class AverageDistanceProcessor extends BaseProcessor {
         List<SessionOfTheDay> sessions = SessionOfTheDay.createSessionsWithInterval(sessionInterval);
         for (SessionOfTheDay session : sessions){
             List<VehicleEntry> entriesInTheSession = getEntriesInTheSession(entries, session);
-            double distance = 0;
-            if (entriesInTheSession.size() > 0) {
-                long timeOfLastVehicle = entriesInTheSession.get(entriesInTheSession.size() - 1).entryTime().getTime();
-                long timeOfFirstVehicle = entriesInTheSession.get(0).entryTime().getTime();
-                double averageTimeGapBetweenVehicles = ((double) timeOfLastVehicle - (double) timeOfFirstVehicle) / (double)entriesInTheSession.size();
-                distance = averageTimeGapBetweenVehicles * App.AVERAGE_SPEED/Time.MILLISECONDS_IN_A_SECOND;
-            }
+            double distance = getAverageDistance(entriesInTheSession);
             sb.append(session + " | Average distance between cars = " + distance + " meters\n");
         }
         return sb.toString();
+    }
+
+    private double getAverageDistance(List<VehicleEntry> entriesInTheSession) {
+        if (entriesInTheSession.size() < 1)
+            return 0;
+            long timeOfLastVehicle = entriesInTheSession.get(entriesInTheSession.size() - 1).entryTime().getTime();
+            long timeOfFirstVehicle = entriesInTheSession.get(0).entryTime().getTime();
+            double averageTimeGapBetweenVehicles = ((double) timeOfLastVehicle - (double) timeOfFirstVehicle) / (double)entriesInTheSession.size();
+            return averageTimeGapBetweenVehicles * App.AVERAGE_SPEED/ Time.MILLISECONDS_IN_A_SECOND;
     }
 }
