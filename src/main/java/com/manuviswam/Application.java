7717503bc84e569ad4d7a83bd577b09e1ae01cf9
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-14 00:08:47 +0530

    Execute all processors with given input

--- src/main/java/com/manuviswam/Application.java
+++ src/main/java/com/manuviswam/Application.java
@@ -1,9 +1,13 @@
 package com.manuviswam;
 
+import com.manuviswam.io.InputReader;
+import com.manuviswam.model.VehicleEntry;
+import com.manuviswam.parser.VehicleEntryParser;
 import com.manuviswam.processors.IDataProcessor;
 
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Application {
@@ -16,8 +20,24 @@ public class Application {
     }
 
     public void run(){
+        List<String> allInputLines = new InputReader(reader).getAllInputLines();
+        if (allInputLines.size() == 0){
+            System.out.println("Error reading inputs. Exiting application...");
+            return;
+        }
 
+        List<VehicleEntry> allEntries = new VehicleEntryParser().parse(allInputLines);
+        if (allEntries.size() == 0){
+            System.out.println("Error parsing inputs. Exiting application...");
+            return;
+        }
+
+        processors.forEach( (key, processor) -> {
+            System.out.println(key);
+            System.out.println(processor.process(allEntries));
+        });
     }
+
 
     public static void main(String[] args) {
 
--- src/test/java/com/manuviswam/ApplicationTest.java
+++ src/test/java/com/manuviswam/ApplicationTest.java
@@ -2,9 +2,12 @@ package com.manuviswam;
 
 import com.manuviswam.model.VehicleEntry;
 import com.manuviswam.processors.IDataProcessor;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.HashMap;
@@ -15,10 +18,17 @@ import static org.junit.Assert.*;
 public class ApplicationTest {
 
     private HashMap<String, IDataProcessor> processors;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
         processors = new HashMap<>();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
     }
 
     @Test
@@ -32,16 +42,60 @@ public class ApplicationTest {
         assertFalse(processor.isInvoked());
     }
 
+    @Test
+    public void shouldCallAllProcessorsIfInputIsValid() throws Exception {
+        Reader reader = new StringReader("A1234\nA1345");
+        FakeProcessor processor = new FakeProcessor("");
+        FakeProcessor processor2 = new FakeProcessor("");
+        processors.put("Processor 1", processor);
+        processors.put("Processor 2", processor2);
+
+        new Application(reader, processors).run();
+
+        assertTrue(processor.isInvoked());
+        assertTrue(processor2.isInvoked());
+    }
+
+    @Test
+    public void shouldDisplayKeysOfAllProcessorsInConsole() throws Exception {
+        Reader reader = new StringReader("A1234\nA1345");
+        FakeProcessor processor = new FakeProcessor("");
+        FakeProcessor processor2 = new FakeProcessor("");
+        processors.put("Processor 1", processor);
+        processors.put("Processor 2", processor2);
+
+        new Application(reader, processors).run();
+
+        assertTrue(outContent.toString().contains("Processor 1"));
+        assertTrue(outContent.toString().contains("Processor 2"));
+    }
+
+    @Test
+    public void shouldDisplayResultsOfAllProcessorsInConsole() throws Exception {
+        Reader reader = new StringReader("A1234\nA1345");
+        FakeProcessor processor = new FakeProcessor("Output of processor 1");
+        FakeProcessor processor2 = new FakeProcessor("Output of processor 1");
+        processors.put("Processor 1", processor);
+        processors.put("Processor 2", processor2);
+
+        new Application(reader, processors).run();
+
+        assertTrue(outContent.toString().contains("Output of processor 1"));
+        assertTrue(outContent.toString().contains("Output of processor 1"));
+    }
+
     class FakeProcessor implements IDataProcessor {
-        private boolean invoked = false;
+        private boolean invoked;
         private String returnValue;
 
         public FakeProcessor(String returnValue) {
+            invoked = false;
             this.returnValue = returnValue;
         }
 
         @Override
         public String process(List<VehicleEntry> entries) {
+            invoked =true;
             return returnValue;
         }
 
