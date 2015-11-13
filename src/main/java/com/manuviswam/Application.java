9139407dc375bf86b8f098d31d551f53714352e0
Manu Viswambharan <manuvisw@thoughtworks.com>
2015-11-14 01:28:25 +0530

    Wire up everything

--- src/main/java/com/manuviswam/Application.java
+++ src/main/java/com/manuviswam/Application.java
@@ -1,10 +1,16 @@
 package com.manuviswam;
 
+import com.manuviswam.constants.App;
 import com.manuviswam.io.InputReader;
 import com.manuviswam.model.VehicleEntry;
 import com.manuviswam.parser.VehicleEntryParser;
 import com.manuviswam.processors.IDataProcessor;
+import com.manuviswam.processors.SpeedDistributionProcessor;
+import com.manuviswam.processors.VehicleCountProcessor;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
@@ -40,6 +46,33 @@ public class Application {
 
 
     public static void main(String[] args) {
+        Reader reader = null;
+        if (args.length == 0) {
+            reader = new InputStreamReader(Application.class.getClassLoader().getResourceAsStream(App.DEFAULT_INPUT_FILE_PATH));
+        }else {
+            try {
+                reader = new FileReader(args[0]);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error reading file : " + args[0] +"\nExiting application...");
+            }
+        }
 
+        int[] intervalsInMinutes = {720, 60, 30, 20, 15};
+        HashMap<String, IDataProcessor> processors = createProcessorsWithIntervals(intervalsInMinutes);
+
+        new Application(reader, processors).run();
+    }
+
+    private static HashMap<String, IDataProcessor> createProcessorsWithIntervals(int[] intervalsInMinutes) {
+        HashMap<String, IDataProcessor> processors = new HashMap<>();
+        for (int interval : intervalsInMinutes){
+            String key = "Vehicle counts in " + interval + " minute intervals";
+            processors.put(key, new VehicleCountProcessor(interval));
+            key = "Speed distribution in " + interval + " minute intervals";
+            processors.put(key, new SpeedDistributionProcessor(interval));
+            key = "Average distance of vehicles in " + interval + " minute intervals";
+            processors.put(key, new SpeedDistributionProcessor(interval));
+        }
+        return processors;
     }
 }
